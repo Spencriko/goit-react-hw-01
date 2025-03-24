9fd0ddb38ebcf1e41faa9cae7678b49ea9cfed24
Spencriko <sofa.aleksandrina1999@gmail.com>
2025-03-24 17:45:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import Profile from './Profile';
+import Profile from './Profile.jsx';
+
 import FriendList from './FriendList';
 import TransactionHistory from './TransactionHistory';
 
